70088f69fb6edba1d10e760f0887b86d9f29c7b7
Park-Jingyeong <pjg0305pjg@gmail.com>
2024-08-16 10:25:35 +0900

    Fix: suspense 수정

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -35,21 +35,21 @@ export default function Home() {
 
   return (
     <>
-      <section>
-        <div className="relative flex h-[240px] w-full justify-center bg-[url('../../public/images/img_main_banner.png')] bg-cover bg-center md:h-[550px]">
-          <div className="relative -left-1/4 mt-[74px] flex flex-col gap-5 md:mt-[144px] xl:mt-[159px]">
-            <h1 className="text-[24px] font-bold leading-tight text-white md:text-[54px] xl:text-[68px]">
-              함께 배우면 즐거운
-              <br /> 스트릿 댄스
-            </h1>
-            <p className="text-[14px] font-bold leading-tight text-white md:text-[20px] xl:text-[24px]">{`${monthNum}월의 인기 체험 BEST 🔥`}</p>
+      <Suspense>
+        <section>
+          <div className="relative flex h-[240px] w-full justify-center bg-[url('../../public/images/img_main_banner.png')] bg-cover bg-center md:h-[550px]">
+            <div className="relative -left-1/4 mt-[74px] flex flex-col gap-5 md:mt-[144px] xl:mt-[159px]">
+              <h1 className="text-[24px] font-bold leading-tight text-white md:text-[54px] xl:text-[68px]">
+                함께 배우면 즐거운
+                <br /> 스트릿 댄스
+              </h1>
+              <p className="text-[14px] font-bold leading-tight text-white md:text-[20px] xl:text-[24px]">{`${monthNum}월의 인기 체험 BEST 🔥`}</p>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <div>
-        <section className="container relative flex justify-center bg-none">
-          <Suspense>
+        <div>
+          <section className="container relative flex justify-center bg-none">
             <div className="absolute -top-14 w-full rounded-2xl bg-white px-6 py-9">
               <h2 className="mb-5 text-lg font-bold md:text-xl">
                 무엇을 체험하고 싶으신가요?
@@ -79,25 +79,25 @@ export default function Home() {
                 </button>
               </div>
             </div>
-          </Suspense>
-        </section>
-        {content ? (
-          <Suspense>
-            <SearchActivityResult />
-          </Suspense>
-        ) : (
-          <div>
-            <section>
-              <PopularActivityList />
-            </section>
-            <section className="container">
-              <Suspense>
-                <ActivityCardList />
-              </Suspense>
-            </section>
-          </div>
-        )}
-      </div>
+          </section>
+          {content ? (
+            <Suspense>
+              <SearchActivityResult />
+            </Suspense>
+          ) : (
+            <div>
+              <section>
+                <PopularActivityList />
+              </section>
+              <section className="container">
+                <Suspense>
+                  <ActivityCardList />
+                </Suspense>
+              </section>
+            </div>
+          )}
+        </div>
+      </Suspense>
     </>
   );
 }
