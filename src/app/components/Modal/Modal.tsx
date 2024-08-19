a1b18bb93035fa0f678fecb04452279419711c5d
MJY1993 <mjylove26@gmail.com>
2024-08-19 10:47:08 +0900

    Feat: Modal 공통컴포넌트 제작

---
+++ src/app/components/Modal/Modal.tsx
@@ -0,0 +1,21 @@
+import { forwardRef, ForwardedRef } from "react";
+
+type ModalProps = {
+  children: React.ReactNode;
+};
+
+export const Modal = forwardRef<HTMLDivElement, ModalProps>(
+  ({ children }, ref: ForwardedRef<HTMLDivElement>) => {
+    return (
+      <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-[#000000B2] px-[24px]">
+        <div ref={ref} className="w-full max-w-[540px] rounded-[8px] bg-white">
+          {children}
+        </div>
+      </div>
+    );
+  },
+);
+
+Modal.displayName = "Modal";
+
+export default Modal;
