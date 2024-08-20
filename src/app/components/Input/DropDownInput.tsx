c19612758da83235dd6eb26a72f7f12e0271cbe4
MJY1993 <mjylove26@gmail.com>
2024-08-20 17:14:32 +0900

    Refactor: CalendarInput과 DropDownInput 사용자 경험 개선(다른 인풋 클릭시 닫히도록)

--- src/app/components/Input/DropDownInput.tsx
+++ src/app/components/Input/DropDownInput.tsx
@@ -3,6 +3,7 @@
 import React, { ForwardedRef, forwardRef, useEffect, useState } from "react";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
+import { useDropdown } from "@hooks/useDropdown";
 import icon_checkmark from "@icons/icon_checkmark.svg";
 import icon_trailing_down from "@icons/icon_trailing_down.svg";
 import icon_trailing_up from "@icons/icon_trailing_up.svg";
@@ -38,7 +39,12 @@ const DropDownInput = forwardRef<HTMLInputElement, DropDownPropsType>(
     },
     ref: ForwardedRef<HTMLInputElement>,
   ) => {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const {
+      isOpen: isDropdownOpen,
+      ref: dropDownRef,
+      toggle,
+      close,
+    } = useDropdown();
     /**
      * dropDownInput 내부에서 관리되는 selectedOption state값입니다.
      * 상위 컴포넌트에서 내려주는 value값이 존재한다면, 그 value값을 <input />에 표시합니다. 그게 아니라면,
@@ -59,36 +65,31 @@ const DropDownInput = forwardRef<HTMLInputElement, DropDownPropsType>(
     }, [dropDownOptions, selectedOption, placeholder]);
 
     return (
-      <div className="relative flex flex-col">
-        <section>
-          <div className="relative flex items-center">
-            <input
-              readOnly
-              ref={ref}
-              onBlur={onBlur}
-              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-              id={id}
-              // <input /> 태그에 표시되는 값은 상위 컴포넌트에서 control되는 value값이 우선순위를 가집니다.
-              value={value || selectedOption}
-              placeholder={!setInitialValue ? placeholder : ""}
-              className={twMerge(
-                `w-full cursor-pointer rounded-[6px] border ${invalid ? "border-red-500" : "border-gray-700"} px-[16px] py-[16px] caret-transparent`,
-                className,
-              )}
-            />
-            <button className="cursor-pointer">
-              <Image
-                src={isDropdownOpen ? icon_trailing_up : icon_trailing_down}
-                alt="Toggle-Dropdown"
-                className="absolute right-[15px] top-[45%]"
-              />
-            </button>
-            {inputLable && (
-              <div className="absolute -top-3 left-5 bg-white text-md">
-                {inputLable}
-              </div>
+      <div ref={dropDownRef} className="relative">
+        <section onClick={() => toggle()} className="flex items-center">
+          <input
+            readOnly
+            ref={ref}
+            onBlur={onBlur}
+            id={id}
+            // <input /> 태그에 표시되는 값은 상위 컴포넌트에서 control되는 value값이 우선순위를 가집니다.
+            value={value || selectedOption}
+            placeholder={!setInitialValue ? placeholder : ""}
+            className={twMerge(
+              `w-full rounded-[6px] border ${invalid ? "border-red-500" : "border-gray-700"} cursor-pointer px-[16px] py-[16px] caret-transparent`,
+              className,
             )}
-          </div>
+          />
+          <Image
+            src={isDropdownOpen ? icon_trailing_up : icon_trailing_down}
+            alt="Toggle-Dropdown"
+            className="absolute right-[15px] top-[45%] cursor-pointer"
+          />
+          {inputLable && (
+            <div className="absolute -top-3 left-5 bg-white text-md">
+              {inputLable}
+            </div>
+          )}
         </section>
         {isDropdownOpen && (
           <section className="absolute top-[60px] z-50 mt-[16px] max-h-[200px] w-full overflow-y-scroll rounded-[6px] bg-white shadow-custom-shadow-01">
@@ -98,7 +99,7 @@ const DropDownInput = forwardRef<HTMLInputElement, DropDownPropsType>(
                   onClick={() => {
                     onChange(option); // react-hook-form의 controller에 값을 전달합니다. // 또는 전달받은 onChange handler에 값을 전달합니다.
                     setSelectedOption(option);
-                    setIsDropdownOpen(false);
+                    close();
                   }}
                   key={index}
                   className="flex gap-[8px] rounded-[6px] px-[12px] py-[8px] font-[400] transition hover:cursor-pointer hover:bg-primary hover:text-white"
