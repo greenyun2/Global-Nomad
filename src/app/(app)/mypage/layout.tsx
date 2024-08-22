208e13d1dee7b3b715500d5de60514d08086a302
MJY1993 <mjylove26@gmail.com>
2024-08-22 14:25:20 +0900

    Refactor: MyActivityList 서버사이드 페치할때 server용 axios 사용하도록 리팩토링

--- src/app/(app)/mypage/layout.tsx
+++ src/app/(app)/mypage/layout.tsx
@@ -1,7 +1,5 @@
-import { getMyActivityList } from "@api/MyActivityStatusApi";
+import getMyActivityListServer from "@app/apiServer/getMyActivityListServer";
 import SideNavigation from "@app/components/SideNavigation/SideNavigation";
-// "next/headers" 에서 제공하는 {cookies}를 사용하면, server-side에서 data-fetching을 하려고 할 때, browser에 저장된 쿠키값에 접근할 수 있는 것 같습니다.
-import { cookies } from "next/headers";
 import MyActivityListContextProvider from "@context/MyActivityListContext";
 
 export default async function MyPageLayout({
@@ -9,14 +7,8 @@ export default async function MyPageLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // HTTP reqeust에 따라온 cookie에 접근하여 accessToken 값을 가져올 수 있습니다.
-  const cookieStore = cookies();
-  const authToken = cookieStore.get("accessToken")?.value;
-  /**
-   * 가져온 accessToken 값을 JWT Token인증이 필요한 api request를 보내는 함수에 전달해줍니다.
-   * 이렇게 하면 server-side에서 data-fetch가 가능합니다.
-   */
-  const myActivityList = await getMyActivityList(authToken);
+  // myActivityList, server-side fetch
+  const myActivityList = await getMyActivityListServer();
 
   return (
     <div className="flex-grow bg-gray-100 py-6 md:py-10 xl:py-[4.5rem]">
