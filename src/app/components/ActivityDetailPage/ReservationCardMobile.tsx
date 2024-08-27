2db3f3b9843ac52d633f306c42d9039dd9b7e30e
MJY1993 <mjylove26@gmail.com>
2024-08-27 14:45:55 +0900

    Feat: 이전 커밋과 내용 동일

--- src/app/components/ActivityDetailPage/ReservationCardMobile.tsx
+++ src/app/components/ActivityDetailPage/ReservationCardMobile.tsx
@@ -297,7 +297,11 @@ export default function ReservationCardMobile({
 
   const tileContent = (date: Date) => {
     const eachDate = format(date, "d");
-    return <div>{eachDate}</div>;
+    const today = new Date();
+    const isPastDate = format(date, "yyyy-MM-dd") < format(today, "yyyy-MM-dd");
+    return (
+      <div className={`${isPastDate && "text-[#a4a1aa]"}`}>{eachDate}</div>
+    );
   };
 
   // tileClassName function to conditionally add the 'react-calendar__tile--active' class
@@ -425,6 +429,7 @@ export default function ReservationCardMobile({
                       }
                       tileDisabled={disabledTiles}
                       tileClassName={tileClassName}
+                      showNeighboringMonth={false}
                     />
                   </div>
                 </div>
